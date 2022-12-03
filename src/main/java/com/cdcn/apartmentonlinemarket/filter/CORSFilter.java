815d8a77d5fbff871298c3cd306f6cab365b7c3f
LE-VAN-KET <anhle1512001@gmail.com>
2022-12-03 15:13:40 +0700

    feat: add cors filter allow origin access

---
+++ src/main/java/com/cdcn/apartmentonlinemarket/filter/CORSFilter.java
@@ -0,0 +1,43 @@
+package com.cdcn.apartmentonlinemarket.filter;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.springframework.data.mapping.Alias.ofNullable;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CORSFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletRequest;
+        HttpServletRequest request = (HttpServletRequest) servletResponse;
+        final String origin = request.getHeader("Origin");
+
+        if (ofNullable(origin).isPresent()) {
+            response.addHeader("Access-Control-Allow-Origin", origin);
+            response.addHeader("Access-Control-Allow-Credentials", "true");
+
+            if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
+                response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+                response.addHeader("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type,Accept,Authorization");
+                response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
+                response.setStatus(200);
+            }
+        }
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+}
